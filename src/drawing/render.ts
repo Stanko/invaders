8a867f6f86ca38ab7065d2258cf440633c1f7560
Stanko <stanko.tadic@gmail.com>
2025-07-31 20:50:40 +0200

    Log ascii version, controls background color

--- src/drawing/render.ts
+++ src/drawing/render.ts
@@ -167,7 +167,7 @@ export default async function render(options: Options): Promise<SVGElement> {
   const iconElement = document.querySelector('link[rel="icon"]') as HTMLLinkElement;
   iconElement.setAttribute('href', iconDataUri);
 
-  // console.log(invader.toString());
+  console.log(invader.toString());
 
   return svgElement;
 }
