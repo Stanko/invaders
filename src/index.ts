c23063bd454e2f0887d3a8009940695a576a42be
Stanko <stanko.tadic@gmail.com>
2025-07-31 23:03:50 +0200

    Goatcounter

--- src/index.ts
+++ src/index.ts
@@ -94,3 +94,12 @@ aboutButton.addEventListener('click', open);
 // Initialize
 buildUI(controls);
 draw();
+
+if (import.meta.env.PROD) {
+  const gc = document.createElement('script');
+  gc.setAttribute('data-goatcounter', 'https://muffinman_io.goatcounter.com/count');
+  gc.setAttribute('async', '');
+  gc.src = '//gc.zgo.at/count.js';
+
+  document.body.appendChild(gc);
+}
